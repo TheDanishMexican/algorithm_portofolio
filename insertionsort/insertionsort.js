f0264c42a39d59c6be0e65a40cc4142b1ec58ec7
TheDanishMexican <danieljchristjansen@gmail.com>
2024-11-08 12:57:43 +0100

    added insertionsort

---
+++ insertionsort/insertionsort.js
@@ -0,0 +1,32 @@
+const list = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
+
+function insertionSortShift(arr) {
+    for (let i = 1; i < arr.length; i++) {
+
+        const key = arr[i];
+
+        let previousIndex = i - 1;
+        let insertIndex = i;
+
+
+        while (previousIndex >= 0 && arr[previousIndex] > key) {
+            let previousElement = arr[previousIndex];
+
+            arr[insertIndex] = previousElement;
+
+            insertIndex--;
+            previousIndex--;
+        }
+
+        arr[insertIndex] = key;
+    }
+
+    return arr;
+}
+
+function insertionSortSwap(arr) {
+
+}
+
+window.list = list;
+window.insertionSortSwap = insertionSortSwap;
