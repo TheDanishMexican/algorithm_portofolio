6779eacac67409495d39596f0843b5036e245ae3
TheDanishMexican <danieljchristjansen@gmail.com>
2024-11-08 14:18:03 +0100

    added iterations variable

--- insertionsort/insertionsort.js
+++ insertionsort/insertionsort.js
@@ -1,10 +1,10 @@
 const list = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
 
 function insertionSortShift(arr) {
+    let iterations = 0;
+
     for (let i = 1; i < arr.length; i++) {
-
-        const key = arr[i];
-
+        let key = arr[i];
         let previousIndex = i - 1;
         let insertIndex = i;
 
@@ -16,15 +16,41 @@ function insertionSortShift(arr) {
 
             insertIndex--;
             previousIndex--;
+            iterations++;
         }
 
         arr[insertIndex] = key;
     }
 
-    return arr;
+    return { arr, iterations };
 }
+
+function insertionSortSwap(arr) {
+    let iterations = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+
+        let key = arr[i];
+        let previousIndex = i - 1;
+
+
+        while (previousIndex >= 0 && arr[previousIndex] > key) {
+            let currentElement = arr[previousIndex + 1];
+
+            arr[previousIndex + 1] = arr[previousIndex];
+            arr[previousIndex] = currentElement;
+
+            previousIndex--;
+            iterations++;
+        }
+    }
+
+    return { arr, iterations };
+}
+
 
 
 
 window.list = list;
 window.insertionSortSwap = insertionSortSwap;
+window.insertionSortShift = insertionSortShift;
